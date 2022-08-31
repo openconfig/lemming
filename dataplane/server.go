cd1b1c9bd3a045b8c981f5b372c66f4170893975
DanG100 <dgrau@google.com>
2022-08-31 20:40:25 +0000

    update port

--- dataplane/server.go
+++ dataplane/server.go
@@ -12,10 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package dataplane is an implementation of the dataplane HAL API.
 package dataplane
 
 import (
 	"context"
+	"fmt"
+	"net"
+
+	"github.com/openconfig/lemming/dataplane/internal/kernel"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	dpb "github.com/openconfig/lemming/proto/dataplane"
 )
@@ -24,17 +31,61 @@ const (
 	Port = 6443
 )
 
-func New() *Server {
-	srv := &Server{}
-
-	return srv
-}
-
 // Server is an implementation of Dataplane HAL API.
 type Server struct {
 	dpb.UnimplementedHALServer
 }
 
-func (s *Server) UpdatePort(context.Context, *dpb.UpdatePortRequest) (*dpb.UpdatePortResponse, error) {
+var (
+	// Stubs for testing
+	setInterfaceHWAddr = kernel.SetInterfaceHWAddr
+	setInterfaceIPs    = kernel.SetInterfaceIPs
+	setInterfaceState  = kernel.SetInterfaceState
+)
 
+// UpdatePort updates an interface properties from the input request.
+func (s *Server) UpdatePort(_ context.Context, req *dpb.UpdatePortRequest) (*dpb.UpdatePortResponse, error) {
+	if req.Name == "" {
+		return nil, status.Error(codes.InvalidArgument, "name not set")
+	}
+	tapName := fmt.Sprintf("%s-tap", req.Name)
+	if req.Hwaddr != "" {
+		addr, err := net.ParseMAC(req.Hwaddr)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "invalid MAC address %q: parse err %v", req.Hwaddr, err)
+		}
+		if err := setInterfaceHWAddr(tapName, addr); err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to set HW addr: %v", err)
+		}
+	}
+	var ips []*net.IPNet
+	for _, ip := range req.Ipv4S {
+		parsedIP := net.ParseIP(ip.GetIp())
+		if parsedIP == nil {
+			return nil, status.Errorf(codes.InvalidArgument, "invalid IP address: %q", ip.GetIp())
+		}
+		ips = append(ips, &net.IPNet{
+			IP:   parsedIP,
+			Mask: net.CIDRMask(int(ip.GetPrefixLen()), 32),
+		})
+	}
+	for _, ip := range req.Ipv6S {
+		parsedIP := net.ParseIP(ip.GetIp())
+		if parsedIP == nil {
+			return nil, status.Errorf(codes.InvalidArgument, "invalid IP address: %q", ip.GetIp())
+		}
+		ips = append(ips, &net.IPNet{
+			IP:   parsedIP,
+			Mask: net.CIDRMask(int(ip.GetPrefixLen()), 128),
+		})
+	}
+	if err := setInterfaceIPs(tapName, ips); err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to set IPs: %v", err)
+	}
+	if req.AdminState != dpb.PortState_PORT_STATE_UNSPECIFIED {
+		if err := setInterfaceState(tapName, req.AdminState == dpb.PortState_PORT_STATE_UP); err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to set interface set: %v", err)
+		}
+	}
+	return &dpb.UpdatePortResponse{}, nil
 }
