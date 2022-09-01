7cbfa97d1d3776b0abcd32da2dbe4a93be3c3ab9
DanG100 <dgrau@google.com>
2022-09-01 23:55:35 +0000

    fix lint

--- gnmi/internal/generate.go
+++ gnmi/internal/generate.go
@@ -1,7 +1,9 @@
 package internal
 
-// Ensure ygnmi generator is included in this go.mod.
-import _ "github.com/openconfig/ygnmi/app/ygnmi/cmd"
+import (
+	// Ensure ygnmi generator is included in this go.mod.
+	_ "github.com/openconfig/ygnmi/app/ygnmi/cmd"
+)
 
 //go:generate ./generate.sh
 
