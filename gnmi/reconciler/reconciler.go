f53e206d1fb5cadae5ee982d2d9c2bb1ac80626f
Daniel Grau <dgrau@google.com>
2022-10-04 10:46:17 -0700

    Add reconciler builders (#65)
    * Add reconciler builders
    
    * feedback
    
    * feedback

--- gnmi/reconciler/reconciler.go
+++ gnmi/reconciler/reconciler.go
@@ -17,10 +17,14 @@ package reconciler
 
 import (
 	"context"
+	"fmt"
 
-	gpb "github.com/openconfig/gnmi/proto/gnmi"
+	"github.com/openconfig/gnmi/errlist"
 	"github.com/openconfig/lemming/gnmi/oc"
 	"github.com/openconfig/ygnmi/ygnmi"
+
+	log "github.com/golang/glog"
+	gpb "github.com/openconfig/gnmi/proto/gnmi"
 )
 
 // Reconciler is a common interface for gNMI reconciler.
@@ -40,4 +44,137 @@ type Reconciler interface {
 	Validate(intendedConfig *oc.Root) error
 	// ValidationPaths returns the set of path prefixes that the reconciler can validate.
 	ValidationPaths() []ygnmi.PathStruct
+}
+
+// Builder simplifies the creation of reconcilers and reduces some of the required boilerplate.
+type Builder struct {
+	br *BuiltReconciler
+}
+
+// NewBuilder creates a new reconciler builder.
+func NewBuilder(id string) *Builder {
+	return &Builder{
+		br: &BuiltReconciler{
+			id: id,
+		},
+	}
+}
+
+// Build returns the reconciler as configuration and resets the builder.
+func (b *Builder) Build() *BuiltReconciler {
+	reconciler := b.br
+	b.br = &BuiltReconciler{}
+	return reconciler
+}
+
+// WithStart appends a new start func to the reconciler.
+func (b *Builder) WithStart(startFn func(context.Context, *ygnmi.Client) error) *Builder {
+	if b.br == nil {
+		b.br = &BuiltReconciler{}
+	}
+	b.br.startFns = append(b.br.startFns, func(ctx context.Context, client gpb.GNMIClient, target string) error {
+		c, err := ygnmi.NewClient(client, ygnmi.WithTarget(target))
+		if err != nil {
+			return fmt.Errorf("failed to build client: %v", err)
+		}
+		return startFn(ctx, c)
+	})
+	return b
+}
+
+// WithValidator appends a validator and validations paths to the reconciler.
+// The Validate func is only called if the SetRequest contains paths which match the paths.
+func (b *Builder) WithValidator(paths []ygnmi.PathStruct, validator func(*oc.Root) error) *Builder {
+	if b.br == nil {
+		b.br = &BuiltReconciler{}
+	}
+	b.br.validateFns = append(b.br.validateFns, validator)
+	b.br.validationPaths = append(b.br.validationPaths, paths...)
+	return b
+}
+
+// TypedBuilder is similar to builder except with a type parameter for use with ygnmi Queries.
+type TypedBuilder[T any] struct {
+	Builder
+}
+
+// NewTypedBuider creates a new reconciler builder.
+func NewTypedBuider[T any](id string) *TypedBuilder[T] {
+	return &TypedBuilder[T]{
+		Builder: Builder{
+			br: &BuiltReconciler{
+				id: id,
+			},
+		},
+	}
+}
+
+// WithWatch adds starting a watch to the reconciler's start funcs.
+func (tb *TypedBuilder[T]) WithWatch(query ygnmi.SingletonQuery[T], predicate func(context.Context, *ygnmi.Client, *ygnmi.Value[T]) error) *TypedBuilder[T] {
+	tb.WithStart(func(ctx context.Context, c *ygnmi.Client) error {
+		watchCtx, cancel := context.WithCancel(ctx)
+		tb.br.stopFns = append(tb.br.stopFns, func() error { cancel(); return nil })
+		w := ygnmi.Watch(watchCtx, c, query, func(v *ygnmi.Value[T]) error {
+			return predicate(watchCtx, c, v)
+		})
+
+		// TODO: handle errors here.
+		go func() {
+			if _, err := w.Await(); err != nil {
+				log.Errorf("Reconciler %q watch err: %v", tb.br.id, err)
+			}
+		}()
+		return nil
+	})
+	return tb
+}
+
+// BuiltReconciler is an implementation of the reconciler interface returned by builders.
+type BuiltReconciler struct {
+	id              string
+	startFns        []func(ctx context.Context, client gpb.GNMIClient, target string) error
+	stopFns         []func() error
+	validateFns     []func(*oc.Root) error
+	validationPaths []ygnmi.PathStruct
+}
+
+func (bt *BuiltReconciler) ID() string {
+	return bt.id
+}
+
+func (bt *BuiltReconciler) Start(ctx context.Context, client gpb.GNMIClient, target string) error {
+	var l errlist.List
+	for _, startFn := range bt.startFns {
+		l.Add(startFn(ctx, client, target))
+	}
+	if err := l.Err(); err != nil {
+		return fmt.Errorf("reconciler %q start errs: %v", bt.id, l.Err())
+	}
+	return nil
+}
+
+func (bt *BuiltReconciler) Stop(context.Context) error {
+	var l errlist.List
+	for _, stopFn := range bt.stopFns {
+		l.Add(stopFn())
+	}
+	if err := l.Err(); err != nil {
+		return fmt.Errorf("reconciler %q stop errs: %v", bt.id, l.Err())
+	}
+	return nil
+}
+
+func (bt *BuiltReconciler) Validate(intendedConfig *oc.Root) error {
+	var l errlist.List
+	for _, validate := range bt.validateFns {
+		l.Add(validate(intendedConfig))
+	}
+	if err := l.Err(); err != nil {
+		return fmt.Errorf("reconciler %q validation errs: %v", bt.id, l.Err())
+	}
+	return nil
+}
+
+func (bt *BuiltReconciler) ValidationPaths() []ygnmi.PathStruct {
+	return bt.validationPaths
 }
---
+++ gnmi/reconciler/reconciler_test.go
@@ -0,0 +1,110 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package reconciler
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/openconfig/gnmi/errdiff"
+	"github.com/openconfig/lemming/gnmi/oc"
+	"github.com/openconfig/lemming/gnmi/oc/ocpath"
+	"github.com/openconfig/ygnmi/ygnmi"
+	"google.golang.org/protobuf/testing/protocmp"
+
+	gpb "github.com/openconfig/gnmi/proto/gnmi"
+)
+
+func TestWithStart(t *testing.T) {
+	tests := []struct {
+		desc string
+		rec  Reconciler
+		want string
+	}{{
+		desc: "single starter",
+		rec: (&Builder{}).WithStart(func(ctx context.Context, c *ygnmi.Client) error {
+			return fmt.Errorf("start 1 err")
+		}).Build(),
+		want: "start 1 err",
+	}, {
+		desc: "2 start funcs",
+		rec: (&Builder{}).WithStart(func(ctx context.Context, c *ygnmi.Client) error {
+			return fmt.Errorf("start 1 err")
+		}).WithStart(func(ctx context.Context, c *ygnmi.Client) error {
+			return fmt.Errorf("start 2 err")
+		}).Build(),
+		want: "start 1 err, start 2 err",
+	}}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			got := tt.rec.Start(context.Background(), nil, "")
+			if diff := errdiff.Check(got, tt.want); diff != "" {
+				t.Fatalf("WithStart failed: got %q want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWithValidator(t *testing.T) {
+	tests := []struct {
+		desc      string
+		rec       Reconciler
+		want      string
+		wantPaths []ygnmi.PathStruct
+	}{{
+		desc: "single validator",
+		rec: (&Builder{}).WithValidator([]ygnmi.PathStruct{ocpath.Root()}, func(r *oc.Root) error {
+			return fmt.Errorf("validator 1")
+		}).Build(),
+		want:      "validator 1",
+		wantPaths: []ygnmi.PathStruct{ocpath.Root()},
+	}, {
+		desc: "2 validate funcs",
+		rec: (&Builder{}).WithValidator([]ygnmi.PathStruct{ocpath.Root()}, func(r *oc.Root) error {
+			return fmt.Errorf("validator 1")
+		}).WithValidator([]ygnmi.PathStruct{ocpath.Root().InterfaceAny()}, func(r *oc.Root) error {
+			return fmt.Errorf("validator 2")
+		}).Build(),
+		want:      "validator 1, validator 2",
+		wantPaths: []ygnmi.PathStruct{ocpath.Root(), ocpath.Root().InterfaceAny()},
+	}}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			gotPaths := tt.rec.ValidationPaths()
+			if diff := cmp.Diff(resolvePaths(t, gotPaths), resolvePaths(t, tt.wantPaths), protocmp.Transform()); diff != "" {
+				t.Fatalf("WithValidate unexpected paths: %s", diff)
+			}
+			got := tt.rec.Validate(nil)
+			if diff := errdiff.Check(got, tt.want); diff != "" {
+				t.Fatalf("WithValidate unexpected error: %s", diff)
+			}
+		})
+	}
+}
+
+func resolvePaths(t testing.TB, paths []ygnmi.PathStruct) []*gpb.Path {
+	t.Helper()
+	protoPaths := make([]*gpb.Path, len(paths))
+	for i, p := range paths {
+		p, _, err := ygnmi.ResolvePath(p)
+		if err != nil {
+			t.Fatal(err)
+		}
+		protoPaths[i] = p
+	}
+	return protoPaths
+}
