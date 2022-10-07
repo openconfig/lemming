7a1d92fd1cfbe353631f7b6a65fa2b9c698bc515
DanG100 <dgrau@google.com>
2022-10-07 16:46:10 +0000

    lint

--- kne/deploytwodut.sh
+++ kne/deploytwodut.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 kne delete ./twodut_topo.pb.txt || true
 cd ..
-DOCKER_BUILDKIT=1 docker build . -t "lemming:latest"
+DOCKER_BUILDKIT=1 docker build . -f Dockerfile.lemming -t "lemming:latest"
 kind load docker-image lemming:latest --name kne
 cd -
 kne create ./twodut_topo.pb.txt 
