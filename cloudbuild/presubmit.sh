04ed8ad62651fe422f57cd8867cae630f22e4329
DanG100 <dgrau@google.com>
2022-10-28 16:26:11 +0000

    test

--- cloudbuild/presubmit.sh
+++ cloudbuild/presubmit.sh
@@ -2,7 +2,15 @@
 
 set -xe
 
+export PATH=${PATH}:/usr/local/go/bin
+GOPATH=$(go env GOPATH)
+export PATH=${PATH}:$GOPATH/bin
+
 cd /tmp/workspace
+# Make sure kne is the version we expect
+go install github.com/openconfig/kne/kne_cli
+mv "$HOME/go/bin/kne_cli" "$HOME/go/bin/kne"
+
 kne deploy ~/kne-internal/deploy/kne/kind-bridge.yaml
 make deploy itest
 make clean
