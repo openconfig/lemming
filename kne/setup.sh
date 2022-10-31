a76026d5c66c231928cd7a234df349c05e3da06a
Daniel Grau <dgrau@google.com>
2022-10-31 13:22:18 -0700

    minor presubmit changes (#91)

--- kne/setup.sh
+++ kne/setup.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname "$0")"
 
-if [[ ! -f $HOME/go/bin/kne ]]
+if ! which kne
 then
     go install github.com/openconfig/kne/kne_cli
     mv "$HOME/go/bin/kne_cli" "$HOME/go/bin/kne"
@@ -13,7 +13,7 @@ DIR=$(pwd)
 cat > config.yaml << EOF
 topology: $DIR/$1
 kubecfg: $HOME/.kube/config
-cli: $HOME/go/bin/kne
+cli: $(which kne)
 username: foo
 password: fake
 EOF
