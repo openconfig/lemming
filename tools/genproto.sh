1d369f7886a78bab66cb42adb835e783e51666da
Daniel Grau <dgrau@google.com>
2023-05-09 22:50:27 +0000

    cache

--- tools/genproto.sh
+++ tools/genproto.sh
@@ -5,8 +5,8 @@ importpath="github.com/openconfig/lemming"
 
 proto_libs=$(bazel query 'kind(go_proto_library, //...)')
 for fulltarget in $proto_libs; do
-    bazel build $fulltarget
+    bazel build "$fulltarget"
     dir=$(sed -E 's/\/\/(.*):.*/\1/g' <<<"$fulltarget")
     target=$(sed -E 's/.*:(.*)_go_proto/\1/g' <<<"$fulltarget")
-    cp -r "$gendir/$dir/$target"_go_proto_/"$importpath/$dir" proto
+    cp -r "$gendir"/"$dir"/"$target"_go_proto_/"$importpath"/"$dir" proto
 done
