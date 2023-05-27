99892d22f6fb1341a019a00fd9af53fad73fabbf
Daniel Grau <dgrau@google.com>
2023-05-27 00:02:12 +0000

    feedback

--- dataplane/forwarding/fwdconfig/action.go
+++ dataplane/forwarding/fwdconfig/action.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package fwdbuilder contains builders for varius proto types.
-package fwdbuilder
+// Package fwdconfig contains builders for varius proto types.
+package fwdconfig
 
 import (
 	"encoding/binary"
