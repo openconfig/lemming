4a2ec62963b224fb4bfcb532deaa8bb4a7876554
Daniel Grau <dgrau@google.com>
2023-06-29 20:26:08 +0000

    fix

--- cloudbuild/fp-test.sh
+++ cloudbuild/fp-test.sh
@@ -2,35 +2,13 @@
 
 
 git clone https://github.com/openconfig/featureprofiles.git
-cd ... || exit
-
-function metadata_kne_topology() {
-  local metadata_test_path
-  metadata_test_path="${1}"
-  local topo_prefix
-  topo_prefix=$(echo "${platform}" | tr "_" "/")
-  declare -A kne_topology_file
-  kne_topology_file["TESTBED_DUT"]="${topo_prefix}/dut.textproto"
-  kne_topology_file["TESTBED_DUT_DUT_4LINKS"]="${topo_prefix}/dutdut.textproto"
-  kne_topology_file["TESTBED_DUT_ATE_2LINKS"]="${topo_prefix}/dutate.textproto"
-  kne_topology_file["TESTBED_DUT_ATE_4LINKS"]="${topo_prefix}/dutate.textproto"
-  kne_topology_file["TESTBED_DUT_ATE_9LINKS_LAG"]="${topo_prefix}/dutate_lag.textproto"
-  for p in "${!kne_topology_file[@]}"; do
-    if grep -q "testbed.*${p}$" "${metadata_test_path}"/metadata.textproto; then
-      echo "${kne_topology_file[${p}]}"
-      return
-    fi
-  done
-  echo "UNKNOWN"
-}
-
-platform="topologies/kne/openconfig/lemming"
+cd featureprofiles || exit
 
 while read -r test_path; do
     kne_topology=$(metadata_kne_topology "${test_path}")
     echo "$kne_topology"
     echo "$test_path"
     kne create "${kne_topology}"
-    go test "./$test_path" -kne-topo "$(pwd)/${kne_topology}" -alsologtostderr
+    go test "./$test_path" -kne-topo "$(pwd)/topologies/kne/openconfig/lemming/topology.textproto " -alsologtostderr
     kne delete "$kne_topology"
 done < fp-tests
