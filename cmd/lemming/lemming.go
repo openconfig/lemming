d3ef78dbdd1d5cca55e92f169065fd15c6b455f3
Daniel Grau <dgrau@google.com>
2023-07-11 21:15:22 +0000

    Add some docs for config

--- cmd/lemming/lemming.go
+++ cmd/lemming/lemming.go
@@ -17,29 +17,29 @@ package main
 import (
 	"flag"
 
-	"github.com/openconfig/lemming"
-	"github.com/openconfig/lemming/sysrib"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 
+	"github.com/openconfig/lemming"
+	"github.com/openconfig/lemming/sysrib"
+
 	log "github.com/golang/glog"
 )
 
 var (
-	gnmiAddr    = flag.String("gnmi", ":9339", "gNMI listen address")
-	gribiAddr   = flag.String("gribi", ":9340", "gRIBI listen address")
-	bgpPort     = flag.Uint("bgp_port", 179, "BGP listening port")
+	gnmiAddr    = pflag.String("gnmi", ":9339", "gNMI listen address")
+	gribiAddr   = pflag.String("gribi", ":9340", "gRIBI listen address")
+	bgpPort     = pflag.Uint("bgp_port", 179, "BGP listening port")
 	target      = pflag.String("target", "fakedut", "name of the fake target")
 	tlsKeyFile  = pflag.String("tls_key_file", "", "Controls whether to enable TLS for gNXI services. If unspecified, insecure credentials are used.")
 	tlsCertFile = pflag.String("tls_cert_file", "", "Controls whether to enable TLS for gNXI services. If unspecified, insecure credentials are used.")
 	zapiAddr    = pflag.String("zapi_addr", sysrib.ZAPIAddr, "Custom ZAPI address: use unix:/tmp/zserv.api for a temp.")
+	_           = pflag.Bool("enable_dataplane", false, "Controls whether to enable dataplane")
 )
 
 func main() {
-	pflag.Bool("enable_dataplane", false, "Controls whether to enable dataplane")
-
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
 	viper.BindPFlags(pflag.CommandLine)
