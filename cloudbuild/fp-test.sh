f3a547df00ebdcf9dd6d9b69e384c909385f160b
Daniel Grau <dgrau@google.com>
2023-07-12 00:53:25 +0000

    Improve fp test script

--- cloudbuild/fp-test.sh
+++ cloudbuild/fp-test.sh
@@ -9,12 +9,12 @@ rc=0
 while read -r test_path; do
     echo "$test_path"
     kne_topology="$(pwd)/topologies/kne/openconfig/lemming/topology.textproto"
-    kne create "${kne_topology}"
+    kne create "${kne_topology}" --stderrthreshold error
     if ! go test "./$test_path" -kne-topo "${kne_topology}" -alsologtostderr; then
-      rc=$?
+      rc=1
       kubectl cluster-info dump --output-directory "/tmp/cluster-log/${test_path/\//-}"  --namespaces openconfig-lemming
     fi
-    kne delete "${kne_topology}"
+    kne delete "${kne_topology}" --stderrthreshold error
     kubectl wait --for=delete namespace/openconfig-lemming --timeout=60s
 done < ../fp-tests
 
