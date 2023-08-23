c105b85a0460059546c61fd867530a8047d24f36
Daniel Grau <dgrau@google.com>
2023-08-23 23:46:24 +0000

    Fix integration tests

--- go.mod
+++ go.mod
@@ -3,22 +3,22 @@ module github.com/openconfig/lemming
 go 1.20
 
 require (
-	cloud.google.com/go/cloudbuild v1.9.0
+	cloud.google.com/go/cloudbuild v1.13.0
 	github.com/go-logr/logr v1.2.3
-	github.com/golang/glog v1.1.1
+	github.com/golang/glog v1.1.2
 	github.com/google/go-cmp v0.5.9
 	github.com/google/gopacket v1.1.19
 	github.com/google/uuid v1.3.0
 	github.com/kentik/patricia v1.2.0
-	github.com/open-traffic-generator/snappi/gosnappi v0.11.15
+	github.com/open-traffic-generator/snappi/gosnappi v0.12.1
 	github.com/openconfig/gnmi v0.10.0
 	github.com/openconfig/gnoi v0.1.0
 	github.com/openconfig/gnsi v1.2.2
-	github.com/openconfig/goyang v1.4.0
+	github.com/openconfig/goyang v1.4.1
 	github.com/openconfig/gribi v1.0.0
 	github.com/openconfig/gribigo v0.0.0-20230207233343-ef59db57c4fc
 	github.com/openconfig/kne v0.1.14
-	github.com/openconfig/ondatra v0.2.5
+	github.com/openconfig/ondatra v0.2.7
 	github.com/openconfig/ygnmi v0.8.7
 	github.com/openconfig/ygot v0.29.9
 	github.com/p4lang/p4runtime v1.4.0-rc.5.0.20220728214547-13f0d02a521e
@@ -31,9 +31,9 @@ require (
 	github.com/wenovus/gobgp/v3 v3.0.0-20230512225508-639b46f0d89c
 	go.uber.org/mock v0.2.0
 	golang.org/x/exp v0.0.0-20230725093048-515e97ebf090
-	golang.org/x/net v0.11.0
-	golang.org/x/sys v0.9.0
-	google.golang.org/api v0.122.0
+	golang.org/x/net v0.14.0
+	golang.org/x/sys v0.11.0
+	google.golang.org/api v0.126.0
 	google.golang.org/grpc v1.58.0-dev
 	google.golang.org/protobuf v1.31.0
 	k8s.io/apimachinery v0.26.3
@@ -43,12 +43,12 @@ require (
 )
 
 require (
-	cloud.google.com/go v0.110.0 // indirect
-	cloud.google.com/go/compute v1.19.1 // indirect
+	cloud.google.com/go v0.110.6 // indirect
+	cloud.google.com/go/compute v1.23.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/iam v0.13.0 // indirect
-	cloud.google.com/go/longrunning v0.4.1 // indirect
-	cloud.google.com/go/pubsub v1.30.0 // indirect
+	cloud.google.com/go/iam v1.1.1 // indirect
+	cloud.google.com/go/longrunning v0.5.1 // indirect
+	cloud.google.com/go/pubsub v1.33.0 // indirect
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
 	github.com/aristanetworks/arista-ceoslab-operator/v2 v2.0.1 // indirect
 	github.com/carlmontanari/difflibgo v0.0.0-20210718194309-31b9e131c298 // indirect
@@ -70,9 +70,9 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/google/s2a-go v0.1.3 // indirect
+	github.com/google/s2a-go v0.1.4 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
-	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.11.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.15 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -97,7 +97,7 @@ require (
 	github.com/osrg/gobgp/v3 v3.14.0 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pborman/uuid v1.2.1 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
@@ -109,20 +109,20 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/srl-labs/srl-controller v0.6.0 // indirect
 	github.com/srl-labs/srlinux-scrapli v0.6.0 // indirect
-	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/vishvananda/netns v0.0.4 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
-	golang.org/x/crypto v0.10.0 // indirect
-	golang.org/x/oauth2 v0.7.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/term v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/term v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230526161137-0005af68ea54 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20230525234035-dd9d682886f9 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230629202037-9506855d4529 // indirect
+	google.golang.org/genproto v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230726155614-23370e0ffb3e // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- integration_tests/onedut_oneotg_tests/static_route_test/static_route_traffic_test.go
+++ integration_tests/onedut_oneotg_tests/static_route_test/static_route_traffic_test.go
@@ -22,16 +22,17 @@ import (
 
 	"github.com/open-traffic-generator/snappi/gosnappi"
 	"github.com/openconfig/gribigo/fluent"
-	"github.com/openconfig/lemming/gnmi/fakedevice"
-	"github.com/openconfig/lemming/internal/binding"
 	"github.com/openconfig/ondatra"
 	"github.com/openconfig/ondatra/gnmi"
 	"github.com/openconfig/ondatra/gnmi/oc"
-	"github.com/openconfig/ondatra/gnmi/oc/ocpath"
 	"github.com/openconfig/ondatra/gnmi/otg/otgpath"
 	"github.com/openconfig/ondatra/otg"
 	"github.com/openconfig/ygnmi/ygnmi"
 	"github.com/openconfig/ygot/ygot"
+
+	"github.com/openconfig/lemming/gnmi/fakedevice"
+	"github.com/openconfig/lemming/internal/attrs"
+	"github.com/openconfig/lemming/internal/binding"
 )
 
 func TestMain(m *testing.M) {
@@ -59,23 +60,8 @@ const (
 	ateIndirectNHCIDRv6 = ateIndirectNHv6 + "/48"
 )
 
-// Attributes bundles some common attributes for devices and/or interfaces.
-// It provides helpers to generate appropriate configuration for OpenConfig
-// and for an ATETopology.  All fields are optional; only those that are
-// non-empty will be set when configuring an interface.
-type Attributes struct {
-	IPv4    string
-	IPv6    string
-	MAC     string
-	Name    string // Interface name, only applied to ATE ports.
-	Desc    string // Description, only applied to DUT interfaces.
-	IPv4Len uint8  // Prefix length for IPv4.
-	IPv6Len uint8  // Prefix length for IPv6.
-	MTU     uint16
-}
-
 var (
-	dutPort1 = Attributes{
+	dutPort1 = attrs.Attributes{
 		Desc:    "dutPort1",
 		IPv4:    "192.0.2.1",
 		IPv4Len: ipv4PrefixLen,
@@ -83,7 +69,7 @@ var (
 		IPv6Len: ipv6PrefixLen,
 	}
 
-	atePort1 = Attributes{
+	atePort1 = attrs.Attributes{
 		Name:    "port1",
 		MAC:     "02:00:01:01:01:01",
 		IPv4:    "192.0.2.2",
@@ -92,7 +78,7 @@ var (
 		IPv6Len: ipv6PrefixLen,
 	}
 
-	dutPort2 = Attributes{
+	dutPort2 = attrs.Attributes{
 		Desc:    "dutPort2",
 		IPv4:    "192.0.2.5",
 		IPv4Len: ipv4PrefixLen,
@@ -100,7 +86,7 @@ var (
 		IPv6Len: ipv6PrefixLen,
 	}
 
-	atePort2 = Attributes{
+	atePort2 = attrs.Attributes{
 		Name:    "port2",
 		MAC:     "02:00:02:01:01:01",
 		IPv4:    "192.0.2.6",
@@ -111,69 +97,35 @@ var (
 )
 
 // configureOTG configures port1 and port2 on the ATE.
-func configureOTG(t *testing.T, otg *otg.OTG) gosnappi.Config {
-	top := otg.NewConfig(t)
+func configureOTG(t *testing.T, ate *ondatra.ATEDevice) gosnappi.Config {
+	top := ate.OTG().NewConfig(t)
 
-	top.Ports().Add().SetName(atePort1.Name)
-	i1 := top.Devices().Add().SetName(atePort1.Name)
-	eth1 := i1.Ethernets().Add().SetName(atePort1.Name + ".Eth").
-		SetPortName(i1.Name()).SetMac(atePort1.MAC)
-	eth1.Ipv4Addresses().Add().SetName(i1.Name() + ".IPv4").
-		SetAddress(atePort1.IPv4).SetGateway(dutPort1.IPv4).
-		SetPrefix(int32(atePort1.IPv4Len))
-	eth1.Ipv6Addresses().Add().SetName(i1.Name() + ".IPv6").
-		SetAddress(atePort1.IPv6).SetGateway(dutPort1.IPv6).
-		SetPrefix(int32(atePort1.IPv6Len))
+	p1 := ate.Port(t, "port1")
+	p2 := ate.Port(t, "port2")
 
-	top.Ports().Add().SetName(atePort2.Name)
-	i2 := top.Devices().Add().SetName(atePort2.Name)
-	eth2 := i2.Ethernets().Add().SetName(atePort2.Name + ".Eth").
-		SetPortName(i2.Name()).SetMac(atePort2.MAC)
-	eth2.Ipv4Addresses().Add().SetName(i2.Name() + ".IPv4").
-		SetAddress(atePort2.IPv4).SetGateway(dutPort2.IPv4).
-		SetPrefix(int32(atePort2.IPv4Len))
-	eth2.Ipv6Addresses().Add().SetName(i2.Name() + ".IPv6").
-		SetAddress(atePort2.IPv6).SetGateway(dutPort2.IPv6).
-		SetPrefix(int32(atePort2.IPv6Len))
+	atePort1.AddToOTG(top, p1, &dutPort1)
+	atePort2.AddToOTG(top, p2, &dutPort2)
+
 	return top
 }
 
-var gatewayMap = map[Attributes]Attributes{
+var gatewayMap = map[attrs.Attributes]attrs.Attributes{
 	atePort1: dutPort1,
 	atePort2: dutPort2,
-}
-
-// configInterfaceDUT configures the interface with the Addrs.
-func configInterfaceDUT(i *oc.Interface, a *Attributes) *oc.Interface {
-	i.Description = ygot.String(a.Desc)
-	i.Type = oc.IETFInterfaces_InterfaceType_ethernetCsmacd
-
-	s := i.GetOrCreateSubinterface(0)
-	s.Enabled = ygot.Bool(true)
-	s4 := s.GetOrCreateIpv4()
-	s4a := s4.GetOrCreateAddress(a.IPv4)
-	s4a.PrefixLength = ygot.Uint8(ipv4PrefixLen)
-	s6 := s.GetOrCreateIpv6()
-	s6a := s6.GetOrCreateAddress(a.IPv6)
-	s6a.PrefixLength = ygot.Uint8(ipv6PrefixLen)
-
-	return i
 }
 
 // configureDUT configures port1 and port2 on the DUT.
 func configureDUT(t *testing.T, dut *ondatra.DUTDevice) {
 	p1 := dut.Port(t, "port1")
-	i1 := &oc.Interface{Name: ygot.String(p1.Name())}
-	gnmi.Replace(t, dut, ocpath.Root().Interface(p1.Name()).Config(), configInterfaceDUT(i1, &dutPort1))
+	gnmi.Replace(t, dut, gnmi.OC().Interface(p1.Name()).Config(), dutPort1.NewOCInterface(p1.Name(), dut))
 
 	p2 := dut.Port(t, "port2")
-	i2 := &oc.Interface{Name: ygot.String(p2.Name())}
-	gnmi.Replace(t, dut, ocpath.Root().Interface(p2.Name()).Config(), configInterfaceDUT(i2, &dutPort2))
+	gnmi.Replace(t, dut, gnmi.OC().Interface(p2.Name()).Config(), dutPort2.NewOCInterface(p2.Name(), dut))
 
-	gnmi.Await(t, dut, ocpath.Root().Interface(dut.Port(t, "port1").Name()).Subinterface(0).Ipv4().Address(dutPort1.IPv4).Ip().State(), time.Minute, dutPort1.IPv4)
-	gnmi.Await(t, dut, ocpath.Root().Interface(dut.Port(t, "port2").Name()).Subinterface(0).Ipv4().Address(dutPort2.IPv4).Ip().State(), time.Minute, dutPort2.IPv4)
-	gnmi.Await(t, dut, ocpath.Root().Interface(dut.Port(t, "port1").Name()).Subinterface(0).Ipv6().Address(dutPort1.IPv6).Ip().State(), time.Minute, dutPort1.IPv6)
-	gnmi.Await(t, dut, ocpath.Root().Interface(dut.Port(t, "port2").Name()).Subinterface(0).Ipv6().Address(dutPort2.IPv6).Ip().State(), time.Minute, dutPort2.IPv6)
+	gnmi.Await(t, dut, gnmi.OC().Interface(dut.Port(t, "port1").Name()).Subinterface(0).Ipv4().Address(dutPort1.IPv4).Ip().State(), time.Minute, dutPort1.IPv4)
+	gnmi.Await(t, dut, gnmi.OC().Interface(dut.Port(t, "port2").Name()).Subinterface(0).Ipv4().Address(dutPort2.IPv4).Ip().State(), time.Minute, dutPort2.IPv4)
+	gnmi.Await(t, dut, gnmi.OC().Interface(dut.Port(t, "port1").Name()).Subinterface(0).Ipv6().Address(dutPort1.IPv6).Ip().State(), time.Minute, dutPort1.IPv6)
+	gnmi.Await(t, dut, gnmi.OC().Interface(dut.Port(t, "port2").Name()).Subinterface(0).Ipv6().Address(dutPort2.IPv6).Ip().State(), time.Minute, dutPort2.IPv6)
 }
 
 func waitOTGARPEntry(t *testing.T) {
@@ -201,7 +153,7 @@ func waitOTGARPEntry(t *testing.T) {
 // testTraffic generates traffic flow from source network to
 // destination network via srcEndPoint to dstEndPoint and checks for
 // packet loss and returns loss percentage as float.
-func testTraffic(t *testing.T, otg *otg.OTG, srcEndPoint, dstEndPoint Attributes, startAddress string, dur time.Duration) float32 {
+func testTraffic(t *testing.T, otg *otg.OTG, srcEndPoint, dstEndPoint attrs.Attributes, startAddress string, dur time.Duration) float32 {
 	waitOTGARPEntry(t)
 	top := otg.FetchConfig(t)
 	top.Flows().Clear().Items()
@@ -233,7 +185,7 @@ func testTraffic(t *testing.T, otg *otg.OTG, srcEndPoint, dstEndPoint Attributes
 // testTrafficv6 generates traffic flow from source network to
 // destination network via srcEndPoint to dstEndPoint and checks for
 // packet loss and returns loss percentage as float.
-func testTrafficv6(t *testing.T, otg *otg.OTG, srcEndPoint, dstEndPoint Attributes, startAddress string, dur time.Duration) float32 {
+func testTrafficv6(t *testing.T, otg *otg.OTG, srcEndPoint, dstEndPoint attrs.Attributes, startAddress string, dur time.Duration) float32 {
 	waitOTGARPEntry(t)
 	top := otg.FetchConfig(t)
 	top.Flows().Clear().Items()
@@ -271,13 +223,13 @@ func awaitTimeout(ctx context.Context, c *fluent.GRIBIClient, t testing.TB, time
 
 // testCounters test packet counters and should be called after testTraffic
 func testCounters(t *testing.T, dut *ondatra.DUTDevice, wantTxPkts, wantRxPkts uint64) {
-	got := gnmi.Get(t, dut, ocpath.Root().Interface(dut.Port(t, "port1").Name()).Counters().InPkts().State())
+	got := gnmi.Get(t, dut, gnmi.OC().Interface(dut.Port(t, "port1").Name()).Counters().InPkts().State())
 	t.Logf("DUT port 1 in-pkts: %d", got)
 	if got < wantTxPkts {
 		t.Errorf("DUT got less packets (%d) than OTG sent (%d)", got, wantTxPkts)
 	}
 
-	got = gnmi.Get(t, dut, ocpath.Root().Interface(dut.Port(t, "port2").Name()).Counters().OutPkts().State())
+	got = gnmi.Get(t, dut, gnmi.OC().Interface(dut.Port(t, "port2").Name()).Counters().OutPkts().State())
 	t.Logf("DUT port 2 out-pkts: %d", got)
 	if got < wantRxPkts {
 		t.Errorf("DUT got sent less packets (%d) than OTG received (%d)", got, wantRxPkts)
@@ -291,7 +243,7 @@ func TestIPv4Entry(t *testing.T) {
 
 	ate := ondatra.ATE(t, "ate")
 	otg := ate.OTG()
-	otgConfig := configureOTG(t, otg)
+	otgConfig := configureOTG(t, ate)
 	otg.PushConfig(t, otgConfig)
 	otg.StartProtocols(t)
 
@@ -391,7 +343,7 @@ func TestIPv4Entry(t *testing.T) {
 	for _, tc := range cases {
 		t.Run(tc.desc, func(t *testing.T) {
 			// Install Static Routes
-			staticp := ocpath.Root().NetworkInstance(fakedevice.DefaultNetworkInstance).
+			staticp := gnmi.OC().NetworkInstance(fakedevice.DefaultNetworkInstance).
 				Protocol(oc.PolicyTypes_INSTALL_PROTOCOL_TYPE_STATIC, fakedevice.StaticRoutingProtocol)
 			for _, route := range tc.routes {
 				gnmi.Replace(t, dut, staticp.Static(*route.Prefix).Config(), route)
