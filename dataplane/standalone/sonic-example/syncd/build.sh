c8d67e40d9a77e6be16e62b2bea77dd0022ca7be
Daniel Grau <dgrau@google.com>
2023-08-31 22:49:03 +0000

    Sample build files, etc for sonic vm

---
+++ dataplane/standalone/sonic-example/syncd/build.sh
@@ -0,0 +1,9 @@
+set -xe
+
+DIR=$(dirname "$0")
+
+rm -f libsai.so
+bazel build //dataplane/standalone:sai
+cp $DIR/../../../../bazel-bin/dataplane/standalone/libsai.so .
+DOCKER_BUILDKIT=1 docker build  . -f $DIR/Dockerfile.syncdlucius -t "docker-syncd:latest"
+docker save docker-syncd:latest -o  $DIR/docker-syncd.tar.gz
---
+++ dataplane/standalone/sonic-example/vm/go.mod
@@ -0,0 +1,10 @@
+module example.com/foo
+
+go 1.20
+
+require github.com/vishvananda/netlink v1.1.0
+
+require (
+	github.com/vishvananda/netns v0.0.4 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
