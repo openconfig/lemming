9b0a0b8c1ca166f3ad3db4f679f0d9896b3d23c6
Daniel Grau <dgrau@google.com>
2023-09-05 18:59:59 +0000

    generate protos

--- tools/genproto.sh
+++ tools/genproto.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -xe
 
 gendir=$(bazel info bazel-genfiles)
-importpath="github.com/openconfig/lemming"
 
 proto_libs=$(bazel query 'kind(go_proto_library, //...)')
 for fulltarget in $proto_libs; do
     bazel build "$fulltarget"
+    importpath=$(bazel query "$fulltarget" --output=build | sed -n 's/.*importpath = "\(.*\)".*/\1/p')
     dir=$(sed -E 's/\/\/(.*):.*/\1/g' <<<"$fulltarget")
     target=$(sed -E 's/.*:(.*)_go_proto/\1/g' <<<"$fulltarget")
     parentdir=$(dirname "$dir")
-    cp -r "$gendir"/"$dir"/"$target"_go_proto_/"$importpath"/"$dir" "$parentdir"
+    cp -r "$gendir"/"$dir"/"$target"_go_proto_/"$importpath" "$parentdir"
 done
