46fc7c36e848e7ba29a93d235b52fbd1a9a5a54d
Daniel Grau <dgrau@google.com>
2023-09-21 21:12:11 +0000

    rbe

--- dataplane/standalone/sonic-example/syncd/build.sh
+++ dataplane/standalone/sonic-example/syncd/build.sh
@@ -3,7 +3,7 @@ set -xe
 DIR=$(dirname "$0")
 
 rm -f libsai.so
-bazel build //dataplane/standalone:sai
+bazel build //dataplane/standalone:sai --config=docker-sandbox
 cp $DIR/../../../../bazel-bin/dataplane/standalone/libsai.so .
 DOCKER_BUILDKIT=1 docker build  . -f $DIR/Dockerfile.syncdlucius -t "docker-syncd:latest"
 docker save docker-syncd:latest -o  $DIR/docker-syncd.tar.gz
