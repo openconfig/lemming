600d27dca010e6ca20203b30d46347d0969ce125
Guo-shiuan Wang <guoshiuan@gmail.com>
2024-07-19 05:43:35 +0000

    lint

--- dataplane/forwarding/protocol/attr.go
+++ dataplane/forwarding/protocol/attr.go
@@ -193,8 +193,8 @@ var FieldAttr = map[fwdpb.PacketFieldNum]struct {
 	},
 	fwdpb.PacketFieldNum_PACKET_FIELD_NUM_L2MC_GROUP_ID: {
 		Sizes: []int{SizeUint64},
-  },
-  fwdpb.PacketFieldNum_PACKET_FIELD_NUM_POLICER_ID: {
+	},
+	fwdpb.PacketFieldNum_PACKET_FIELD_NUM_POLICER_ID: {
 		Sizes: []int{SizeUint64},
 	},
 }
