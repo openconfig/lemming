c766b19e6a4d7eb5c80aab500b3eb62c7eb0e38c
Daniel Grau <dgrau@google.com>
2024-09-27 19:01:24 +0000

    test

--- dataplane/kernel/genetlink/genetlink.c
+++ dataplane/kernel/genetlink/genetlink.c
@@ -19,7 +19,7 @@ enum {
 
 struct nl_sock** nlsocks = NULL;
 int family_id;
-int create_index = 0;
+int create_idx = 0;
 const int max_sockets = 16;
 
 int create_port(const char* family, const char* group) {
@@ -27,36 +27,34 @@ int create_port(const char* family, const char* group) {
   if (nlsocks == NULL) {
     nlsocks = malloc(sizeof(struct nl_sock*) * max_sockets);
   }
-  if (create_index >= max_sockets) {
+  if (create_idx >= max_sockets) {
     return -1;
   }
 
-  struct nl_sock* nlsock = nlsocks[create_index];
-
-  nlsock = nl_socket_alloc();
-  if (nlsock == NULL) {
+  nlsocks[create_idx] = nl_socket_alloc();
+  if (nlsocks[create_idx] == NULL) {
     return -1;
   }
-  nl_socket_disable_auto_ack(nlsock);
-  int error = genl_connect(nlsock);
+  nl_socket_disable_auto_ack(nlsocks[create_idx]);
+  int error = genl_connect(nlsocks[create_idx]);
   if (error < 0) {
-    nl_socket_free(nlsock);
+    nl_socket_free(nlsocks[create_idx]);
     return -1;
   }
-  family_id = genl_ctrl_resolve(nlsock, family);
+  family_id = genl_ctrl_resolve(nlsocks[create_idx], family);
   if (family_id < 0) {
-    nl_socket_free(nlsock);
+    nl_socket_free(nlsocks[create_idx]);
     return -1;
   }
-  int group_id = genl_ctrl_resolve_grp(nlsock, family, group);
+  int group_id = genl_ctrl_resolve_grp(nlsocks[create_idx], family, group);
   if (group_id < 0) {
-    nl_socket_free(nlsock);
+    nl_socket_free(nlsocks[create_idx]);
     return -1;
   }
 
-  nl_socket_set_peer_groups(nlsock, (1 << (group_id - 1)));
+  nl_socket_set_peer_groups(nlsocks[create_idx], (1 << (group_id - 1)));
 
-  return create_index++;
+  return create_idx++;
 }
 
 int send_packet(int sock_idx, const void* pkt, uint32_t size, int in_ifindex,
@@ -65,13 +63,13 @@ int send_packet(int sock_idx, const void* pkt, uint32_t size, int in_ifindex,
   fprintf(stderr,"populating packet to index %d\n", sock_idx);
   struct nl_msg* msg = nlmsg_alloc();
   genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, family_id, 0, 0, 0, 1);
-  // printf("putting src if index nl msg: %d", in_ifindex);
+  // fprintf(stderr, "putting src if index nl msg: %d\n", in_ifindex);
   // NLA_PUT_S16(msg, GENL_PACKET_ATTR_IIFINDEX, in_ifindex);
-  // printf("putting dst if index nl msg: %d", out_ifindex);
+  // fprintf(stderr, "putting dst if index nl msg: %d\n", out_ifindex);
   // NLA_PUT_S16(msg, GENL_PACKET_ATTR_OIFINDEX, out_ifindex);
-  // printf("putting context nl msg: %d", context);
+  // fprintf(stderr, "putting context nl msg: %d\n", context);
   // NLA_PUT_U32(msg, GENL_PACKET_ATTR_CONTEXT, context);
-  // printf("putting data nl msg, size: %d", size);
+  // fprintf(stderr, "putting data nl msg, size: %d\n", size);
   // NLA_PUT(msg, GENL_PACKET_ATTR_DATA, size, pkt);
   fprintf(stderr,"sending to index %d\n", sock_idx);
   if (nl_send(nlsocks[sock_idx], msg) < 0) {
